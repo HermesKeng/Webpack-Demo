2e45c22635639a77fd042695ff1db5ff95547ea7
Hermes <kaining1101@gmail.com>
2021-07-11 14:51:15 -0700

    Add font and image in webpack

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 import _ from "lodash";
 import "./style.scss";
+import MyImage from "./myimage.png";
 function component() {
   const element = document.createElement("div");
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+  element.innerHTML = _.join(["Hello", "webpack 大家好，我在拉後雅"], " ");
   element.classList.add("hello");
+
+  const myImage = new Image();
+  myImage.src = MyImage;
+
+  element.appendChild(myImage);
   return element;
 }
 
