bf0eecffb8c243d8dea603ccce8e168828549db1
Hermes <kaining1101@gmail.com>
2021-07-11 22:22:22 -0700

    Upload webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,64 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
-  entry: path.resolve(__dirname, "src", "index.tsx"),
-  mode: "development",
+  entry: path.resolve(__dirname, 'src', 'index.tsx'),
+  mode: 'development',
   output: {
-    filename: "[name].bundle.js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
     clean: true,
   },
-  devtool: "inline-source-map",
+  devtool: 'inline-source-map',
   devServer: {
-    contentBase: "./dist",
+    contentBase: './dist',
   },
   resolve: {
     extensions: [
-      ".tsx",
-      ".ts",
-      ".js",
-      ".scss",
-      ".sass",
-      ".jsx",
-      ".csv",
-      ".png",
-      ".jpg",
-      ".jpeg",
-      ".gif",
-      ".otf",
+      '.tsx',
+      '.ts',
+      '.js',
+      '.scss',
+      '.sass',
+      '.jsx',
+      '.csv',
+      '.png',
+      '.jpg',
+      '.jpeg',
+      '.gif',
+      '.otf',
     ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "public", "index.html"),
+      template: path.resolve(__dirname, 'public', 'index.html'),
     }),
   ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
+        use: ['csv-loader'],
       },
       {
         test: /\.xml$/i,
-        use: ["xml-loader"],
+        use: ['xml-loader'],
       },
     ],
   },
