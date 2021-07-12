5c0fbb7723da5e5cb858b0d73625bd4bbd3815de
Hermes <kaining1101@gmail.com>
2021-07-11 18:14:24 -0700

    Change the start point and add @type/react to the webpack setting

---
+++ src/index.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+import ReactDOM from "react-dom";
+const styles = require("./style.scss");
+
+export interface MyComponentProps {}
+
+export default class MyComponent extends React.Component<MyComponentProps, {}> {
+  render() {
+    return (
+      <div className={"test"}>
+        <h1>Hello World!</h1>
+        <p className="btn btn-primary">Foo to the barz</p>
+      </div>
+    );
+  }
+}
+
+const App = () => {
+  return <h1>Hello Wepack!</h1>;
+};
+ReactDOM.render(<MyComponent />, document.getElementById("root"));
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 module.exports = {
-  entry: { index: "./src/index.ts" },
+  entry: path.resolve(__dirname, "src", "index.tsx"),
   mode: "development",
   output: {
     filename: "[name].bundle.js",
@@ -39,10 +39,6 @@ module.exports = {
         test: /\.tsx?$/,
         use: "ts-loader",
         exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        use: ["babel-loader"],
       },
       {
         test: /\.s[ac]ss$/i,
